124f92ea3608c6eeb808ca7597086e2dbe5d5b68
zuoqin <zuoqin.wz@alibaba-inc.com>
2017-11-24 14:28:27 +0800

    Log : 添加sts 初始化方式

--- Sample/ObjectiveCDemo001/ViewController.m
+++ Sample/ObjectiveCDemo001/ViewController.m
@@ -20,15 +20,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    NSString * ENDPOINT = @"******";
+    NSString * PROJECTNAME = @"******";
+    NSString * LOGSTORENAME = @"******";
+    
     //        移动端是不安全环境，不建议直接使用阿里云主账号ak，sk的方式。建议使用STS方式。具体参见 https://help.aliyun.com/document_detail/62643.html
     //        注意：只建议在测试环境或者用户可以保证阿里云主账号AK，SK安全的前提下使用。
     //        通过主账号AK，SK使用日志服务
-    NSString * ENDPOINT = @"******";
-    NSString * AK = @"******";
-    NSString * SK = @"******";
-    NSString * PROJECTNAME = @"******";
-    NSString * LOGSTORENAME = @"******";
-    LOGClient * client = [[LOGClient alloc] initWithEndPoint:ENDPOINT accessKeyID:AK accessKeySecret:SK projectName:PROJECTNAME];
+    
+    //    NSString * AK = @"******";
+    //    NSString * SK = @"******";
+    //    LOGClient * client = [[LOGClient alloc] initWithEndPoint:ENDPOINT accessKeyID:AK accessKeySecret:SK projectName:PROJECTNAME];
+    
+    //通过STS使用日志服务
+    NSString * STS_AK = @"******";
+    NSString * STS_SK = @"******";
+    NSString * STS_TOKEN = @"******";
+    
+    LOGClient * client = [[LOGClient alloc] initWithEndPoint:ENDPOINT accessKeyID:STS_AK accessKeySecret:STS_SK token:STS_TOKEN projectName:PROJECTNAME];
+    
     //  log调试开关
     client.mIsLogEnable = true;
     
@@ -37,7 +47,7 @@
     
     LogGroup * group = [[LogGroup alloc] initWithTopic:@"topic" source:@"object-c"];
     [group PutLog:loginfo];
-
+    
     [client PostLog:group logStoreName:LOGSTORENAME call:^(NSURLResponse *response,NSError *error) {
         NSLog(@"response %@", [response debugDescription]);
         NSLog(@"error %@",[error debugDescription]);
