e03d6fc8f56b630b9fb057169607ec3f44719f50
zuoqin <zuoqin.wz@alibaba-inc.com>
2019-01-10 21:34:01 +0800

    Log : fix os sample log

--- Sample/ObjectiveCDemo001/ViewController.m
+++ Sample/ObjectiveCDemo001/ViewController.m
@@ -34,7 +34,6 @@ NSString * LOGSTORENAME = @"LOGSTORENAME";
 }
 
 - (void)setupClient {
-    SLSConfig *clientConfig = [[SLSConfig alloc] initWithConnectType:SLSConnectionTypeWifi cachable:YES];
 //        移动端是不安全环境，不建议直接使用阿里云主账号ak，sk的方式。建议使用STS方式。具体参见 https://help.aliyun.com/document_detail/62681.html
 //        注意：只建议在测试环境或者用户可以保证阿里云主账号AK，SK安全的前提下使用。
 //        通过主账号AK，SK使用日志服务
@@ -49,12 +48,12 @@ NSString * LOGSTORENAME = @"LOGSTORENAME";
     NSString * STS_SK = @"STS_SK";
     NSString * STS_TOKEN = @"STS_TOKEN";
     
-    _client = [[LOGClient alloc] initWithEndPoint:ENDPOINT accessKeyID:STS_AK accessKeySecret:STS_SK projectName:PROJECTNAME token:nil config:clientConfig];
+    _client = [[LOGClient alloc] initWithEndPoint:ENDPOINT accessKeyID:STS_AK accessKeySecret:STS_SK projectName:PROJECTNAME token:nil];
     
     
     
     //  log调试开关
-    _client.mIsLogEnable = true;
+    _client.mIsLogEnable = false;
 }
 
 - (IBAction)sendLogAction:(id)sender {
