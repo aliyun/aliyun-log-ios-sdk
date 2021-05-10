65b2041d01b359def12bf00688891412c69c9020
chaolee50 <chaolee50@outlook.com>
2021-05-10 14:43:27 +0800

    2.2.12

--- AliyunLogProducer/AliyunLogProducer/LogProducerConfig.m
+++ AliyunLogProducer/AliyunLogProducer/LogProducerConfig.m
@@ -24,7 +24,7 @@
 
 @implementation LogProducerConfig
 
-static NSString *VERSION = @"sls-ios-sdk_v2.2.11";
+static NSString *VERSION = @"sls-ios-sdk_v2.2.12";
 static NSInteger LocalServerDeltaTime = 0;
 NSLock *TimeLock;
 
