3d67a0eac6543c9ba5f07968cf7f2d6c81943768
yulong.gyl <yulong.gyl@alibaba-inc.com>
2021-12-17 14:09:58 +0800

    feat: add log simple example

--- Xcode/AliyunLogDemo/ViewController.h
+++ Xcode/AliyunLogDemo/ViewController.h
@@ -6,9 +6,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AliyunLogProducer/AliyunLogProducer.h>
 
 @interface ViewController : UIViewController
+{
+    @private
+    LogProducerConfig *_config;
+    LogProducerClient *_client;
+}
 
+- (void) initLogProducer;
 
 @end
 
