dbf1adc89dd5cfa351e53bd820d7e0dd7ddd78a3
yulong.gyl <yulong.gyl@alibaba-inc.com>
2021-12-21 19:07:35 +0800

    feat: add crash mock code

---
+++ Xcode/AliyunLogDemo/Example/Crash/CrashExampController.h
@@ -0,0 +1,16 @@
+//
+//  CrashExampController.h
+//  AliyunLogDemo
+//
+//  Created by gordon on 2021/12/21.
+//
+
+#import "ExampleViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CrashExampController : ExampleViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Xcode/AliyunLogDemo/ViewController.h
+++ Xcode/AliyunLogDemo/ViewController.h
@@ -14,6 +14,8 @@
 
 - (UIButton *) createButton: (NSString *) title andAction: (SEL) action andX: (CGFloat) x andY: (CGFloat) y;
 
+- (UIButton *) createButton: (NSString *) title andAction: (SEL) action andX: (CGFloat) x andY: (CGFloat) y andWidth: (CGFloat) width andHeight: (CGFloat) height;
+
 - (UILabel *) createLabel: (NSString *)title andX: (CGFloat) x andY: (CGFloat) y;
 
 - (UILabel *) createLabel: (NSString *) title andX: (CGFloat) x andY: (CGFloat) y andWidth: (CGFloat) width andHeight: (CGFloat) height;
