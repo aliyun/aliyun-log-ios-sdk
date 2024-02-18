65ea49a3daab8d069dad60ae65cb0373adbc56f5
yulong.gyl <yulong.gyl@alibaba-inc.com>
2024-02-18 16:19:25 +0800

    refine: remove outdate publish scripts

--- publish/build-frameworks.sh
+++ publish/build-frameworks.sh
@@ -5,10 +5,6 @@ rm -rf build
 mkdir build
 
 sh build-producer-framework.sh
-sh build-ot-framework.sh
-sh build-ot-swift-framework.sh
-sh build-core-framework.sh
-sh build-crashreporter-framework.sh
 sh build-unity4sls.sh
 sh build-slsipa4unity.sh
 sh build-wpkmobi.sh
