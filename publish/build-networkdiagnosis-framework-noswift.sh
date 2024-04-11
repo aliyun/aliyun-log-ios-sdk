86b022d6f725e891d32fedc34adecacc76523086
yulong.gyl <yulong.gyl@alibaba-inc.com>
2024-04-11 19:38:24 +0800

    feat: refine local publish scripts

--- publish/build-networkdiagnosis-framework-noswift.sh
+++ publish/build-networkdiagnosis-framework-noswift.sh
@@ -30,10 +30,38 @@ cp -r iphoneos/${SCHEME_SHADOW}.framework ./
 lipo -remove arm64 ./iphonesimulator/${SCHEME_SHADOW}.framework/${SCHEME_SHADOW} -output ./iphonesimulator/${SCHEME_SHADOW}.framework/${SCHEME_SHADOW}
 lipo -create iphoneos/${SCHEME_SHADOW}.framework/${SCHEME_SHADOW} iphonesimulator/${SCHEME_SHADOW}.framework/${SCHEME_SHADOW} -output ${SCHEME_SHADOW}.framework/${SCHEME_SHADOW}
 
+pushd ${SCHEME_SHADOW}.framework
+export _version_=$VERSION
+
 # set framework version
-/usr/libexec/PlistBuddy -c "Set CFBundleVersion $VERSION" ${SCHEME_SHADOW}.framework/Info.plist
-/usr/libexec/PlistBuddy -c "Set CFBundleShortVersionString $VERSION" ${SCHEME_SHADOW}.framework/Info.plist
-/usr/libexec/PlistBuddy -c "Set :MinimumOSVersion 100.0" ${SCHEME_SHADOW}.framework/Info.plist
+/usr/libexec/PlistBuddy -c "Set CFBundleVersion $VERSION" -c "Set CFBundleShortVersionString $VERSION" Info.plist
+
+# compatible for Xcode 15.3
+# https://github.com/Azure/azure-spatial-anchors-samples/issues/407#issuecomment-1755051174
+# Check if the MinimumOSVersion key exists in the Info.plist
+if /usr/libexec/PlistBuddy -c "Print :MinimumOSVersion" Info.plist >/dev/null 2>&1; then
+  # Key exists, so update its value
+  /usr/libexec/PlistBuddy -c "Set :MinimumOSVersion 100.0" Info.plist
+fi
+
+# compatible for xcprivacy
+find . -name "*.bundle" -type d -exec sh -c '
+    for bundle in "$PWD/$@"; do
+        echo "bundle path: $bundle"
+        find "$bundle" -name "Info.plist" -exec /usr/libexec/PlistBuddy \
+            -c "Set :CFBundleVersion $_version_" \
+            -c "Set :CFBundleShortVersionString $_version_" \
+            -c "Delete :CFBundleExecutable $_version_" "{}" \;
+            
+        for file in "$bundle"/*; do
+            if [[ "$file" != */Info.plist ]] && [[ "$file" != */PrivacyInfo.xcprivacy ]]; then
+                echo "deleting file: $file"
+                rm -rf "$file"
+            fi
+        done
+    done
+' sh {} +
+popd
 
 rm -rf iphoneos
 rm -rf iphonesimulator
--- publish/build-networkdiagnosis-framework.sh
+++ publish/build-networkdiagnosis-framework.sh
@@ -30,10 +30,38 @@ cp -r iphoneos/${SCHEME}.framework ./
 lipo -remove arm64 ./iphonesimulator/${SCHEME}.framework/${SCHEME} -output ./iphonesimulator/${SCHEME}.framework/${SCHEME}
 lipo -create iphoneos/${SCHEME}.framework/${SCHEME} iphonesimulator/${SCHEME}.framework/${SCHEME} -output ${SCHEME}.framework/${SCHEME}
 
+pushd ${SCHEME}.framework
+export _version_=$VERSION
+
 # set framework version
-/usr/libexec/PlistBuddy -c "Set CFBundleVersion $VERSION" ${SCHEME}.framework/Info.plist
-/usr/libexec/PlistBuddy -c "Set CFBundleShortVersionString $VERSION" ${SCHEME}.framework/Info.plist
-/usr/libexec/PlistBuddy -c "Set :MinimumOSVersion 100.0" ${SCHEME}.framework/Info.plist
+/usr/libexec/PlistBuddy -c "Set CFBundleVersion $VERSION" -c "Set CFBundleShortVersionString $VERSION" Info.plist
+
+# compatible for Xcode 15.3
+# https://github.com/Azure/azure-spatial-anchors-samples/issues/407#issuecomment-1755051174
+# Check if the MinimumOSVersion key exists in the Info.plist
+if /usr/libexec/PlistBuddy -c "Print :MinimumOSVersion" Info.plist >/dev/null 2>&1; then
+  # Key exists, so update its value
+  /usr/libexec/PlistBuddy -c "Set :MinimumOSVersion 100.0" Info.plist
+fi
+
+# compatible for xcprivacy
+find . -name "*.bundle" -type d -exec sh -c '
+    for bundle in "$PWD/$@"; do
+        echo "bundle path: $bundle"
+        find "$bundle" -name "Info.plist" -exec /usr/libexec/PlistBuddy \
+            -c "Set :CFBundleVersion $_version_" \
+            -c "Set :CFBundleShortVersionString $_version_" \
+            -c "Delete :CFBundleExecutable $_version_" "{}" \;
+            
+        for file in "$bundle"/*; do
+            if [[ "$file" != */Info.plist ]] && [[ "$file" != */PrivacyInfo.xcprivacy ]]; then
+                echo "deleting file: $file"
+                rm -rf "$file"
+            fi
+        done
+    done
+' sh {} +
+popd
 
 rm -rf iphoneos
 rm -rf iphonesimulator
